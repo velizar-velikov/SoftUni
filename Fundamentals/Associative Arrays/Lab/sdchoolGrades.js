0d93cacb1d37715ec15228fd39983ffac3e852df
velizar-velikov <vvelikov7.22@gmail.com>
2023-10-18 23:43:10 +0300

    sorted output alphabetically

--- Fundamentals/Associative Arrays/Lab/sdchoolGrades.js
+++ Fundamentals/Associative Arrays/Lab/sdchoolGrades.js
@@ -12,11 +12,12 @@ function schoolGrages(input) {
             studentGrades.set(name, oldGrades);
         }
     } 
-    let sortedStudentGradesByName = new Map(Array.from(studentGrades).sort((a, b) => a[0] - b[0]));
-    console.log(studentGrades);
-    console.log(sortedStudentGradesByName);
+    let sortedStudentGradesByName = new Map(Array.from(studentGrades).sort((a, b) => a[0].localeCompare(b[0])));
+    for (let [name, grades] of sortedStudentGradesByName) {
+        console.log(`${name}: ${(grades.reduce((sum, grade) => sum + grade, 0) / grades.length).toFixed(2)}`);
+    }
 }
-schoolGrages(['Lilly 4 6 6 5',
-'Tim 5 6',
-'Tammy 2 4 3',
-'Tim 6 6'])
+schoolGrages(['Steven 3 5 6 4',
+'George 4 6',
+'Tammy 2 5 3',
+'Steven 6 3'])
