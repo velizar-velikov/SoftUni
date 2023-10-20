e8b273c71f28695c54f67333e42206b26975f879
velizar-velikov <vvelikov7.22@gmail.com>
2023-10-20 19:34:35 +0300

    tried fixing hearthDelivery

--- Fundamentals/Old Exams/04. Programming Fundamentals Mid Exam/heartDelivery.js
+++ Fundamentals/Old Exams/04. Programming Fundamentals Mid Exam/heartDelivery.js
@@ -1,19 +1,18 @@
 function heartDelivery(input) {
     let houses = input.shift().split('@').map(Number);
 
-    let command = input.shift();
     let indexOfHouse = 0;
+    let command = input.shift();
     while (command != 'Love!') {
         let tokens = command.split(' ');
         let jumpLength = Number(tokens[1]);
-        if (indexOfHouse + jumpLength <= houses.length - 1) {
-            indexOfHouse += jumpLength;
-        } else {
+        indexOfHouse += jumpLength;
+        if (indexOfHouse > houses.length - 1) {
             indexOfHouse = 0;
         }
         if (houses[indexOfHouse] == 0) {
             console.log(`Place ${indexOfHouse} already had Valentine's day.`);
-        } else if (houses[indexOfHouse] - 2 >= 0) {
+        } else {
             houses[indexOfHouse] -= 2;
             if (houses[indexOfHouse] == 0) {
                 console.log(`Place ${indexOfHouse} has Valentine's day.`);
@@ -21,14 +20,14 @@ function heartDelivery(input) {
         }
         command = input.shift();
     }
-    console.log(`Cupid's last position was ${indexOfHouse}.`);
     let noValentinesCount = 0;
-    for (let house of houses) {
-        if (house != 0) {
+    for (let item of houses) {
+        if (item != 0) {
             noValentinesCount++;
         }
     }
-
+    
+    console.log(`Cupid's last position was ${indexOfHouse}.`);
     if (noValentinesCount == 0) {
         console.log('Mission was successful');
     } else {
