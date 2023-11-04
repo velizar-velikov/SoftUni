143529e4684e336f6db1df7932b08ef94c791333
velizar-velikov <vvelikov7.22@gmail.com>
2023-11-04 13:49:04 +0200

    solved legendaryFarming

---
+++ Fundamentals/Associative Arrays/Exercise/legendaryFarming.js
@@ -0,0 +1,36 @@
+function legendaryFarming(str) {
+    let legendaries = { shards: 'Shadowmourne', fragments: 'Valanyr', motes: 'Dragowrath' };
+    let keyMaterials = { shards: 0, fragments: 0, motes: 0 };
+    let junkMaterials = {};
+    let keyMaterialsTypes = ['shards', 'fragments', 'motes']
+
+    let tokens = str.toLowerCase().split(' ');
+
+    for (let i = 0; i < tokens.length; i += 2) {
+        let qty = Number(tokens[i]);
+        let material = tokens[i + 1];
+
+        if (keyMaterialsTypes.includes(material)) {
+            keyMaterials[material] += qty;
+        } else {
+            if (junkMaterials.hasOwnProperty(material)) {
+                junkMaterials[material] += qty;
+            } else {
+                junkMaterials[material] = qty;
+            }
+        }
+
+        if (keyMaterials[material] >= 250) {
+            keyMaterials[material] -= 250;
+            console.log(`${legendaries[material]} obtained!`);
+            break;
+        }
+    }
+
+    let sortedKeyMatEntries = Object.entries(keyMaterials).sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]));
+    let sortedJunkMatEntries = Object.entries(junkMaterials).sort((a, b) => a[0].localeCompare(b[0]));
+
+    sortedKeyMatEntries.forEach(([material, qty]) => console.log(`${material}: ${qty}`));
+    sortedJunkMatEntries.forEach(([material, qty]) => console.log(`${material}: ${qty}`));
+}
+legendaryFarming('123 silver 6 shards 8 shards 5 motes 9 fangs 75 motes 103 MOTES 8 Shards 86 Motes 7 stones 19 silver')
