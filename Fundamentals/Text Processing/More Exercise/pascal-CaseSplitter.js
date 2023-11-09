14f766f646678ddefb6ebfc89c727f15ae587b37
velizar-velikov <vvelikov7.22@gmail.com>
2023-11-09 20:41:45 +0200

    fixed a bug in my solution and passed with 100/10

--- Fundamentals/Text Processing/More Exercise/pascal-CaseSplitter.js
+++ Fundamentals/Text Processing/More Exercise/pascal-CaseSplitter.js
@@ -4,24 +4,22 @@ function pascalCaseSplitter(str) {
     for (let i = 0; i < str.length; i++) {
         let char = str[i];
         if (char == char.toUpperCase()) {
-            if (word.length > 0 && word !== word.toLowerCase()) {
+            if (word.length > 0) {
                 words.push(word);
             }
-            word = char;
-        } else if (i == str.length - 1) {
-            word += char;
-            if (word !== word.toLowerCase() && word !== word.toUpperCase()) {
-                words.push(word);
-            }
-        } else {
-            word += char;
+            word = '';
         }
-    }
+        word += char;
+    } if (word.length > 0) {
+        // if (word !== word.toLowerCase() && word !== word.toUpperCase()) {
+        // }
+        words.push(word);
 
-    if (words.length > 0) {
         console.log(words.join(', '));
+
     }
 }
+
 pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan')
 pascalCaseSplitter('HoldTheDoor')
 pascalCaseSplitter('ThisIsSoAnnoyingToDo')
