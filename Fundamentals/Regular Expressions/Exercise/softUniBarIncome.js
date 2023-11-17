8506da5d0bc8fe161764f502dc717fc278a76579
velizar-velikov <vvelikov7.22@gmail.com>
2023-11-17 15:09:33 +0200

    deleted some coomented-out code

--- Fundamentals/Regular Expressions/Exercise/softUniBarIncome.js
+++ Fundamentals/Regular Expressions/Exercise/softUniBarIncome.js
@@ -10,19 +10,15 @@ function calcIncome(input) {
         console.log(`${match.groups.name}: ${match.groups.product} - ${money.toFixed(2)}`);
     })
 
-        // let money = (match.groups.price * match.groups.count);
-        // totalMoney += money;
-        // console.log(`${match.groups.name}: ${match.groups.product} - ${money.toFixed(2)}`);
-
     console.log(`Total income: ${totalMoney.toFixed(2)}`);
 }
 calcIncome(['%George%<Croissant>|2|10.3$',
-'%Peter%<Gum>|1|1.3$',
-'%Maria%<Cola>|1|2.4$',
-'end of shift'])
+    '%Peter%<Gum>|1|1.3$',
+    '%Maria%<Cola>|1|2.4$',
+    'end of shift'])
 
 calcIncome(['%InvalidName%<Croissant>|2|10.3$',
-'%Peter%<Gum>1.3$',
-'%Maria%<Cola>|1|2.4',
-'%Valid%<Valid>valid|10|valid20$',
-'end of shift'])
+    '%Peter%<Gum>1.3$',
+    '%Maria%<Cola>|1|2.4',
+    '%Valid%<Valid>valid|10|valid20$',
+    'end of shift'])
