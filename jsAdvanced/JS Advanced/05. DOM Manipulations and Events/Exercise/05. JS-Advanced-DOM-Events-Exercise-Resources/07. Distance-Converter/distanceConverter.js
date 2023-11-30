621413775e184bd5a799587118226448930bb29e
velizar-velikov <vvelikov7.22@gmail.com>
2023-11-30 20:14:34 +0200

    solved 07 and 08 from dom manipulation and events ex

--- jsAdvanced/JS Advanced/05. DOM Manipulations and Events/Exercise/05. JS-Advanced-DOM-Events-Exercise-Resources/07. Distance-Converter/distanceConverter.js
+++ jsAdvanced/JS Advanced/05. DOM Manipulations and Events/Exercise/05. JS-Advanced-DOM-Events-Exercise-Resources/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,25 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+
+    function convertDistance() {
+        let unitsToMeter = {
+            km: 1000,
+            m: 1,
+            cm: 0.01,
+            mm: 0.001,
+            mi: 1609.34,
+            yrd: 0.9144,
+            ft: 0.3048,
+            in: 0.0254
+        }
+        let inputUnits = document.getElementById('inputUnits').value;
+        let outputUnits = document.getElementById('outputUnits').value;
+
+        let inputDistanceEl = document.getElementById('inputDistance');
+        let outputDistanceEl = document.getElementById('outputDistance');
+
+        outputDistanceEl.value = Number(inputDistanceEl.value) * unitsToMeter[inputUnits] / unitsToMeter[outputUnits];
+    }
+
+    let convertBtn = document.getElementById('convert');
+    convertBtn.addEventListener('click', convertDistance);
 }
--- jsAdvanced/JS Advanced/05. DOM Manipulations and Events/Exercise/05. JS-Advanced-DOM-Events-Exercise-Resources/08. Sudomu/solution.js
+++ jsAdvanced/JS Advanced/05. DOM Manipulations and Events/Exercise/05. JS-Advanced-DOM-Events-Exercise-Resources/08. Sudomu/solution.js
@@ -1,4 +1,59 @@
 function solve() {
+    let inputs = document.querySelectorAll('tbody input');
+    let buttons = document.getElementsByTagName('button');
+    let checkBtn = buttons[0];
+    let clearBtn = buttons[1];
+    let table = document.querySelector('table');
+    let para = document.querySelector('#check p');
 
-    //TODO...
+    checkBtn.addEventListener('click', checkInputs);
+    clearBtn.addEventListener('click', clearInputs);
+
+    function checkInputs() {
+        let inputValuesArr = Array.from(inputs).map(input => input.value);
+        let sums = [];
+        let isInvalid = false;
+
+        for (let i = 0; i < 3; i++) {
+            let verticalSum = 0;
+            let horSum = 0;
+            let horSet = new Set();
+            let verticalSet = new Set();
+            for (let j = 0; j < 3; j++) {
+                let horNum = Number(inputValuesArr[i * 3 + j]);
+                let verticalNum = Number(inputValuesArr[i + j * 3]);
+                horSum += horNum;
+                verticalSum += verticalNum;
+                horSet.add(horNum);
+                verticalSet.add(verticalNum);
+            }
+            sums.push(horSum);
+            sums.push(verticalSum);
+            if (horSet.size !== 3 || verticalSet.size !== 3) {
+                isInvalid = true;
+            }
+        }
+        let firstSum = sums[0];
+        if (inputValuesArr.includes('') || sums.some(sum => sum != firstSum)) {
+            isInvalid = true;
+        }
+        if (isInvalid == false) {
+            table.style.border = '2px solid green';
+            para.textContent = 'You solve it! Congratulations!';
+            para.style.color = 'green';
+        } else {
+            table.style.border = '2px solid red';
+            para.textContent = 'NOP! You are not done yet...';
+            para.style.color = 'red';
+        }
+
+    }
+    function clearInputs() {
+        for (let i = 0; i < 9; i++) {
+            inputs[i].value = '';
+        }
+        table.style.border = 'initial';
+        para.style.color = 'initial';
+        para.textContent = '';
+    }
 }
