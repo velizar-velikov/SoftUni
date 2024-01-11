7ca3dc6018191e593210388ef480631fc99955a1
velizar-velikov <vvelikov7.22@gmail.com>
2024-01-11 13:04:23 +0200

    add export to object

--- jsAdvanced/JS Advanced/Old Exams/JS Advanced Retake Exam - 6 December 2023/03.Pet-Adoption-Agency_Resources/petAdoptionAgency.js
+++ jsAdvanced/JS Advanced/Old Exams/JS Advanced Retake Exam - 6 December 2023/03.Pet-Adoption-Agency_Resources/petAdoptionAgency.js
@@ -1,45 +1,48 @@
 const petAdoptionAgency = {
-    isPetAvailable(pet, availableCount, vaccinated) {
-      if (typeof pet !== "string" || typeof availableCount !== "number" ||typeof vaccinated !== "boolean") {
-        throw new Error("Invalid input");
+  isPetAvailable(pet, availableCount, vaccinated) {
+    if (typeof pet !== "string" || typeof availableCount !== "number" || typeof vaccinated !== "boolean") {
+      throw new Error("Invalid input");
+    }
+
+    if (availableCount <= 0) {
+      return `Sorry, there are no ${pet}(s) available for adoption at the agency.`;
+    } else {
+      if (vaccinated) {
+        return `Great! We have ${availableCount} vaccinated ${pet}(s) available for adoption at the agency.`;
+      } else {
+        return `Great! We have ${availableCount} ${pet}(s) available for adoption, but they need vaccination.`;
       }
 
-      if (availableCount <= 0) {
-        return `Sorry, there are no ${pet}(s) available for adoption at the agency.`;
-      } else {
-        if(vaccinated){
-            return `Great! We have ${availableCount} vaccinated ${pet}(s) available for adoption at the agency.`;
-        }else{
-            return `Great! We have ${availableCount} ${pet}(s) available for adoption, but they need vaccination.`;
-        }
-        
+    }
+  },
+  getRecommendedPets(petList, desiredTraits) {
+    let recommendedPets = [];
+
+    if (!Array.isArray(petList) || typeof desiredTraits !== "string") {
+      throw new Error("Invalid input");
+    }
+
+    petList.forEach((pet) => {
+      if (pet.traits === desiredTraits) {
+        recommendedPets.push(pet.name);
       }
-    },
-    getRecommendedPets(petList, desiredTraits) {
-      let recommendedPets = [];
-  
-      if (!Array.isArray(petList) || typeof desiredTraits !== "string") {
-        throw new Error("Invalid input");
-      }
-  
-      petList.forEach((pet) => {
-        if (pet.traits === desiredTraits) {
-          recommendedPets.push(pet.name);
-        }
-      });
-  
-      if (recommendedPets.length === 0) {
-        return `Sorry, we currently have no recommended pets with the desired traits: ${desiredTraits}.`;
-      } else {
-        return `Recommended pets with the desired traits (${desiredTraits}): ${recommendedPets.join(", ")}`;
-      }
-    },
-    adoptPet(pet, adopterName) {
-      if (typeof pet !== "string" || typeof adopterName !== "string") {
-        throw new Error("Invalid input");
-      }
-  
-      return `Congratulations, ${adopterName}! You have adopted ${pet} from the agency. Enjoy your time with your new furry friend!`;
-    },
-  };
- 
+    });
+
+    if (recommendedPets.length === 0) {
+      return `Sorry, we currently have no recommended pets with the desired traits: ${desiredTraits}.`;
+    } else {
+      return `Recommended pets with the desired traits (${desiredTraits}): ${recommendedPets.join(", ")}`;
+    }
+  },
+  adoptPet(pet, adopterName) {
+    if (typeof pet !== "string" || typeof adopterName !== "string") {
+      throw new Error("Invalid input");
+    }
+
+    return `Congratulations, ${adopterName}! You have adopted ${pet} from the agency. Enjoy your time with your new furry friend!`;
+  },
+};
+
+export { petAdoptionAgency };
+
+console.log(petAdoptionAgency.getRecommendedPets([{ name: 'cat', traits: 'jumps' }, { name: 'dog', traits: 'runs' }], 'walks'));
