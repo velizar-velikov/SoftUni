a3995795ca30c5e4cca757639682f421e55409c6
velizar-velikov <vvelikov7.22@gmail.com>
2024-01-20 01:17:05 +0200

    solved report.js

--- jsAdvanced/JS Advanced/04. DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ jsAdvanced/JS Advanced/04. DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,25 @@
 function generateReport() {
-    //TODO
+
+    const rows = document.querySelectorAll('tbody tr');
+    const output = [];
+
+    for (let row of rows) {
+
+        const tds = row.children;
+        const obj = {};
+
+        for (let i = 0; i < tds.length; i++) {
+
+            const headCell = document.querySelectorAll('thead input')[i];
+            if (headCell.checked) {
+                obj[headCell.getAttribute('name')] = tds[i].textContent;
+            }
+        }
+        if (Object.keys(obj).length > 0) {
+            output.push(obj);
+        }
+    }
+    if (output.length > 0) {
+        document.getElementById('output').value = JSON.stringify(output);
+    }
 }
