07559be5b8d78f22da8c7caad9f7e89bbb734881
velizar-velikov <vvelikov7.22@gmail.com>
2024-01-20 02:05:44 +0200

    decimal to binary/hexadecimal converter logic

--- jsAdvanced/JS Advanced/04. DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
+++ jsAdvanced/JS Advanced/04. DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
@@ -1,4 +1,33 @@
 function solve() {
 
-    //TODO...
+    const input = document.getElementById('input');
+    const selectTo = document.getElementById('selectMenuTo');
+    const resultEl = document.getElementById('result');
+
+    addOptions(selectTo);
+    document.querySelector('button').addEventListener('click', onConvert);
+
+    function onConvert() {
+        const selectedOption = Array.from(document.querySelectorAll('#selectMenuTo option'))
+            .find(option => option.selected);
+        resultEl.value = parseNum(Number(input.value), selectedOption.getAttribute('value'));
+    }
+
+    function parseNum(num, type) {
+        return type == 'binary' ?
+            num.toString(2) :
+            num.toString(16).toUpperCase();
+    }
+
+    function addOptions(selectTo) {
+        const option1 = document.createElement('option');
+        option1.value = 'binary';
+        option1.textContent = 'Binary';
+        const option2 = document.createElement('option');
+        option2.value = 'hexadecimal';
+        option2.textContent = 'Hexadecimal';
+        selectTo.innerHTML = '';
+        selectTo.appendChild(option1);
+        selectTo.appendChild(option2);
+    }
 }
