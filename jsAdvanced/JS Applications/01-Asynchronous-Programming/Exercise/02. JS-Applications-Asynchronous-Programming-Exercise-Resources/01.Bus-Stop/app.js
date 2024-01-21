d58e360ba2515a5148f632dd02971b8e770e8e18
velizar-velikov <vvelikov7.22@gmail.com>
2024-01-21 20:54:10 +0200

    solved 01.Bus Stop

--- jsAdvanced/JS Applications/01-Asynchronous-Programming/Exercise/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ jsAdvanced/JS Applications/01-Asynchronous-Programming/Exercise/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,31 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+
+    const stopId = document.getElementById('stopId').value;
+    const stopName = document.getElementById('stopName');
+    const busesList = document.getElementById('buses');
+    busesList.innerHTML = '';
+
+    try {
+
+        const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
+        if (!response.ok) {
+            const error = new Error();
+            error.status = response.status;
+            error.statusText = response.statusText;
+            throw error;
+        }
+
+        const data = await response.json();
+
+        stopName.textContent = data.name;
+
+        for (let [busId, time] of Object.entries(data.buses)) {
+            const li = document.createElement('li');
+            li.textContent = `Bus ${busId} arrives in ${time} minutes`;
+            busesList.appendChild(li);
+        }
+    }
+    catch (error) {
+        stopName.textContent = 'Error';
+    }
 }
