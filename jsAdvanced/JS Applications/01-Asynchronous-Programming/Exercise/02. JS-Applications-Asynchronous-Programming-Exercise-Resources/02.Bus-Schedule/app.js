dd9c0d3b5b7ffbaa908858fb6457cdea6e0e3778
velizar-velikov <vvelikov7.22@gmail.com>
2024-01-21 20:54:42 +0200

    solved 02.Bus Schedule

--- jsAdvanced/JS Applications/01-Asynchronous-Programming/Exercise/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
+++ jsAdvanced/JS Applications/01-Asynchronous-Programming/Exercise/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
@@ -1,11 +1,43 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+
+    const infoBox = document.getElementById('info');
+    let stopId = 'depot';
+    let stopName;
+
+    async function depart() {
+
+        try {
+            let response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${stopId}`);
+
+            if (!response.ok) {
+                const error = new Error();
+                error.status = response.status;
+                error.statusText = response.statusText;
+                throw error;
+            }
+
+            const data = await response.json();
+            stopName = data.name;
+            stopId = data.next;
+            infoBox.textContent = `Next stop ${stopName}`;
+
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        }
+        catch (error) {
+            infoBox.textContent = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.textContent = `Arriving at ${stopName}`;
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
     }
 
     return {
