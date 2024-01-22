b5de60d5ee464d200bc3909528d26db2d900ef52
velizar-velikov <vvelikov7.22@gmail.com>
2024-01-22 19:13:20 +0200

    successfully solved 07.Hell's Kitchen

--- jsAdvanced/JS Advanced/04. DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ jsAdvanced/JS Advanced/04. DOM Introduction/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,21 +1,18 @@
-//unsuccessfull solution
-
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick() {
 
-      const bestRestaurantEl = document.querySelector('#bestRestaurant span');
-      const bestRestaurantWokersEl = document.querySelector('#workers span');
+      const bestRestaurantEl = document.querySelector('#bestRestaurant p');
+      const bestRestaurantWokersEl = document.querySelector('#workers p');
 
       const textArea = document.querySelector('textarea');
-      const input = textArea.value.substring(2, textArea.value.length - 2).split('[, \n]+]');
-      console.log(input);
+      const input = JSON.parse(textArea.value);
 
       const restaurants = {};
 
       input.forEach(info => {
-         let [restaurant, workersInfoStr] = info.split('-');
+         let [restaurant, workersInfoStr] = info.split(' - ');
          if (!restaurants.hasOwnProperty(restaurant)) {
             restaurants[restaurant] = {
                workers: {},
@@ -37,22 +34,23 @@ function solve() {
                   (Object.keys(restaurants[restaurant].workers).length - 1) + salary) /
                   Object.keys(restaurants[restaurant].workers).length;
 
-               bestSalary = salary > bestSalary ? salary : bestSalary;
+               restaurants[restaurant].bestSalary = salary > restaurants[restaurant].bestSalary ?
+                  salary :
+                  restaurants[restaurant].bestSalary;
             }
          })
-
-         const bestSalaryRestaurantEntry = Object.entries(restaurants).sort((a, b) => b.avgSalary - a.avgSalary)[0];
-
-         bestRestaurantEl.textContent = `Name: ${bestSalaryRestaurantEntry[0]} Average Salary: ${bestSalaryRestaurantEntry[1].avgSalary} Best Salary: ${bestSalaryRestaurantEntry[1].bestSalary}`;
-
-         const workersInBestRestaurant = bestSalaryRestaurantEntry[1].workers;
-
-         let workersOutput = [];
-         Object.entries(workersInBestRestaurant).sort((a, b) => b[1] - a[1]).forEach(([name, salary]) => {
-            workersOutput.push(`Name: ${name} With Salary: ${salary}`);
-         });
-         bestRestaurantWokersEl.textContent = workersOutput.join(' ');
       })
+
+      const bestSalaryRestaurantEntry = Object.entries(restaurants).sort((a, b) => b[1].avgSalary - a[1].avgSalary)[0];
+      bestRestaurantEl.textContent = `Name: ${bestSalaryRestaurantEntry[0]} Average Salary: ${bestSalaryRestaurantEntry[1].avgSalary.toFixed(2)} Best Salary: ${bestSalaryRestaurantEntry[1].bestSalary.toFixed(2)}`;
+
+      const workersInBestRestaurant = bestSalaryRestaurantEntry[1].workers;
+
+      let workersOutput = [];
+      Object.entries(workersInBestRestaurant).sort((a, b) => b[1] - a[1]).forEach(([name, salary]) => {
+         workersOutput.push(`Name: ${name} With Salary: ${salary}`);
+      });
+      bestRestaurantWokersEl.textContent = workersOutput.join(' ');
 
       textArea.value = '';
    }
