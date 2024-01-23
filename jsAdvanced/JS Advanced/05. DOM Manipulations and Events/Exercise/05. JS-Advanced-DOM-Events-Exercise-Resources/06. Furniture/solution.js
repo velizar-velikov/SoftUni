215134d556ac9c9c7ff3da6d49a0b3e4a627c6a4
velizar-velikov <vvelikov7.22@gmail.com>
2024-01-23 17:58:21 +0200

    resolved 06.Furniture correctly

--- jsAdvanced/JS Advanced/05. DOM Manipulations and Events/Exercise/05. JS-Advanced-DOM-Events-Exercise-Resources/06. Furniture/solution.js
+++ jsAdvanced/JS Advanced/05. DOM Manipulations and Events/Exercise/05. JS-Advanced-DOM-Events-Exercise-Resources/06. Furniture/solution.js
@@ -1,100 +1,61 @@
 function solve() {
 
-  let textAreas = document.getElementsByTagName('textarea');
-  let textArea = textAreas[0];
+    const input = document.querySelector('textarea');
+    const outputTextarea = document.querySelector('textarea:last-of-type');
+    const buttons = document.querySelectorAll('button');
+    const generateBtn = buttons[0];
+    const buyBtn = buttons[1];
 
-  let furnitureLists = JSON.parse(textArea.value);
+    generateBtn.addEventListener('click', addRows);
+    buyBtn.addEventListener('click', showShoppingInfo);
 
-  let buttons = document.getElementsByTagName('button');
-  let generateBtn = buttons[0];
-  let buyBtn = buttons[1];
-  debugger
+    function addRows() {
 
-  function handleGenerate() {
-    for (let furnitureList of furnitureLists) {
-      
-      let image = furnitureList['img'];
-      let name = furnitureList['name'];
-      let price = furnitureList['price'];
-      let decFactor = furnitureList['decFactor'];
-      createAddRow(image, name, price, decFactor);
+        const inputArr = JSON.parse(input.value);
+        for (let obj of inputArr) {
+
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                          <td><img src="${obj.img}"></td>
+                          <td><p>${obj.name}</p></td>
+                          <td><p>${obj.price}</p></td>
+                          <td><p>${obj.decFactor}</p></td>
+                          <td><input type="checkbox" disabled /></td>`;
+
+            document.querySelector('table tbody').appendChild(tr);
+        }
+        input.value = '';
+        enableCheckboxes();
     }
-    textArea.value = '';
-  }
 
-  generateBtn.addEventListener('click', handleGenerate);
-  buyBtn.addEventListener('click', handleBuy);
-
-  function createAddRow(image, name, price, decFactor) {
-    let row = document.createElement('tr');
-  
-    let cell1 = document.createElement('td');
-    let img = document.createElement('img');
-    img.src = image;
-    cell1.appendChild(img);
-  
-    let cell2 = document.createComment('td');
-    let p1 = document.createElement('p');
-    p1.textContent = name;
-    cell2.appendChild(p1);
-  
-    let cell3 = document.createElement('td');
-    let p2 = document.createElement('p');
-    p2.textContent = price;
-    cell3.appendChild(p2);
-  
-    let cell4 = document.createElement('td');
-    let p3 = document.createElement('p');
-    p3.textContent = decFactor;
-    cell4.appendChild(p3);
-  
-    let cell5 = document.createElement('td');
-    let check = document.createElement('input');
-    check.type = 'checkbox';
-    check.checked = false;
-    cell5.appendChild(check);
-  
-    row.appendChild(cell1);
-    row.appendChild(cell2);
-    row.appendChild(cell3);
-    row.appendChild(cell4);
-    row.appendChild(cell5);
-
-    let tableBody = document.querySelector('tbody');
-    tableBody.appendChild(row);
-
-  }
-
-
-  function handleBuy() {
-    let rows = document.querySelectorAll('tbody tr');
-
-    let boughtFurniture = [];
-    let totalPrice = 0;
-    let decFactors = [];
-    for (let row of rows) {
-      let checkbox = row.querySelector('input');
-      if (checkbox.checked == true) {
-        let cells = row.querySelectorAll('td');
-        let furniture = cells[1];
-        let furnitureName = furniture.textContent;
-        boughtFurniture.push(furnitureName);
-
-        let price = cells[2].textContent;
-        totalPrice += Number(price);
-
-        let decFactor = cells[3].textContent;
-        decFactors.push(decFactor);
-      }
+    function enableCheckboxes() {
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        Array.from(checkboxes).forEach(checkbox => checkbox.disabled = false);
     }
-    let avgDecFactor = decFactors
-      .map(Number)
-      .reduce((acc, value) => acc + value, 0) / decFactors.length;
 
-      let outputTextarea = document.querySelectorAll('button')[1];
+    function showShoppingInfo() {
+        const { furniture, totalPrice, totalDecFactor } = getShoppingInfo();
 
-    outputTextarea.value += `Bought furniture: ${boughtFurniture.join(', ')}\n`;
-    outputTextarea.value += `Total price: ${totalPrice.toFixed(2)}\n`;
-    outputTextarea.value += `Average decoration factor: ${avgDecFactor}`;
-  }
+        let output = `Bought furniture: ${furniture.join(', ')}`;
+        output += `\nTotal price: ${totalPrice.toFixed(2)}\n`;
+        output += `Average decoration factor: ${totalDecFactor / furniture.length}`;
+        outputTextarea.value = output;
+    }
+
+    function getShoppingInfo() {
+        const trs = document.querySelectorAll('tbody tr');
+        const furniture = [];
+        let totalPrice = 0;
+        let totalDecFactor = 0;
+
+        for (let tr of trs) {
+            if (tr.querySelector('input[type="checkbox"]').checked) {
+                const paras = tr.querySelectorAll('p');
+                furniture.push(paras[0].textContent);
+                totalPrice += Number(paras[1].textContent);
+                totalDecFactor += Number(paras[2].textContent);
+            }
+        }
+        return { furniture, totalPrice, totalDecFactor };
+    }
 }
