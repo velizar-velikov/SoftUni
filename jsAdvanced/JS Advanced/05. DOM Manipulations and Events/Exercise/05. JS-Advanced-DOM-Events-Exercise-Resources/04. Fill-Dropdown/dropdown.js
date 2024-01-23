478e8fb17619456044882b133b88fe8270fa6ab5
velizar-velikov <vvelikov7.22@gmail.com>
2024-01-23 17:56:41 +0200

    Update dropdown.js

--- jsAdvanced/JS Advanced/05. DOM Manipulations and Events/Exercise/05. JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
+++ jsAdvanced/JS Advanced/05. DOM Manipulations and Events/Exercise/05. JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
@@ -1,11 +1,12 @@
 function addItem() {
-    let newTextInput = document.getElementById('newItemText');
-    let newValueInput = document.getElementById('newItemValue');
-    let newOption = document.createElement('option');
+    const newTextInput = document.getElementById('newItemText');
+    const newValueInput = document.getElementById('newItemValue');
+    const newOption = document.createElement('option');
+
     newOption.textContent = newTextInput.value;
     newOption.value = newValueInput.value;
-    let select = document.getElementById('menu');
-    select.appendChild(newOption);
+
+    document.getElementById('menu').appendChild(newOption);
     newTextInput.value = '';
     newValueInput.value = '';
 }
