c282e35cf93193135736819f961b8f0b0aecd9c4
velizar-velikov <vvelikov7.22@gmail.com>
2024-01-25 01:02:44 +0200

    solved 02.Phone Book

--- jsAdvanced/JS Applications/02-Remote-Data and-Authentication/Exercise/03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
+++ jsAdvanced/JS Applications/02-Remote-Data and-Authentication/Exercise/03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
@@ -1,5 +1,107 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    const phonebookList = document.getElementById('phonebook');
+    const personInput = document.getElementById('person');
+    const phoneInput = document.getElementById('phone');
+    const inputs = [personInput, phoneInput];
+
+    document.getElementById('btnLoad').addEventListener('click', loadContacts);
+    document.getElementById('btnCreate').addEventListener('click', createContact);
+    phonebookList.addEventListener('click', deletePhone);
+
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+
+    async function loadContacts() {
+
+        try {
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                const error = await response.json();
+                throw error;
+            }
+
+            const phonebookData = await response.json();
+            phonebookList.innerHTML = '';
+            appendListItems(phonebookData);
+        } catch (error) {
+            console.log(`Error: ${error.message}`);
+        }
+    }
+
+    async function createContact() {
+
+        if (checkInputs() == false) {
+            alert('All inputs are required!')
+            return;
+        }
+
+        const contactData = {
+            person: personInput.value,
+            phone: phoneInput.value
+        }
+        const options = {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(contactData)
+        }
+        try {
+            const response = await fetch(url, options);
+
+            if (!response.ok) {
+                const error = await response.json();
+                throw error;
+            }
+            clearInputs();
+            loadContacts();
+
+        } catch (error) {
+            console.log(`Error: ${error.message}`);
+        }
+    }
+
+    async function deletePhone(event) {
+        const element = event.target;
+        if (element.textContent == 'Delete') {
+            element.parentElement.remove();
+        }
+        const key = element.parentElement.id;
+
+        try {
+            const response = await fetch(`${url}/${key}`, {
+                method: 'delete'
+            });
+
+            if (!response.ok) {
+                const error = await response.json();
+                throw error;
+            }
+        } catch (error) {
+            console.log(`Error: ${error.message}`);
+        }
+    }
+
+    function appendListItems(phonebookData) {
+
+        for (let obj of Object.values(phonebookData)) {
+            const li = document.createElement('li');
+            li.id = obj._id;
+            li.textContent = `${obj.person}: ${obj.phone}`;
+
+            const button = document.createElement('button');
+            button.textContent = 'Delete';
+            li.appendChild(button);
+            phonebookList.appendChild(li);
+        }
+    }
+
+    function checkInputs() {
+        return inputs.every(input => input.value.length > 0);
+    }
+
+    function clearInputs() {
+        inputs.forEach(input => input.value = '');
+    }
 }
 
 attachEvents();
