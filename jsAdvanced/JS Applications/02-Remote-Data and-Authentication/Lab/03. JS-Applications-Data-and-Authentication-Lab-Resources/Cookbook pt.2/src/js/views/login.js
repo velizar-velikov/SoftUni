a34094d121acbb7541522d4401077127592394f3
velizar-velikov <vvelikov7.22@gmail.com>
2024-02-24 21:47:07 +0200

    Create login.js

---
+++ jsAdvanced/JS Applications/02-Remote-Data and-Authentication/Lab/03. JS-Applications-Data-and-Authentication-Lab-Resources/Cookbook pt.2/src/js/views/login.js
@@ -0,0 +1,23 @@
+import { post } from "../api/api.js";
+
+const formElement = document.querySelector('form');
+formElement.addEventListener('submit', loginUser);
+
+async function loginUser(e) {
+    e.preventDefault();
+
+    const formData = new FormData(formElement);
+
+    const { email, password } = Object.fromEntries(formData.entries());
+
+    if (email == '' || password == '') {
+        alert('All fields are required!');
+        return;
+    }
+
+    const response = await post('users/login', { email, password });
+
+    sessionStorage.setItem('user', JSON.stringify(response));
+    location.href = '../../index.html';
+}
+
