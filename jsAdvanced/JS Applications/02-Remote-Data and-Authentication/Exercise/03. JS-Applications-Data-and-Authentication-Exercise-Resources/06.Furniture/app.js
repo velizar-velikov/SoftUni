f66aa1fb1ef6790fc230a7f0ca6d5f235adf4ee6
velizar-velikov <vvelikov7.22@gmail.com>
2024-02-26 23:03:12 +0200

    extract logout function in a seperate file

--- jsAdvanced/JS Applications/02-Remote-Data and-Authentication/Exercise/03. JS-Applications-Data-and-Authentication-Exercise-Resources/06.Furniture/app.js
+++ jsAdvanced/JS Applications/02-Remote-Data and-Authentication/Exercise/03. JS-Applications-Data-and-Authentication-Exercise-Resources/06.Furniture/app.js
@@ -1,33 +1,16 @@
 import { showAllOrders } from "./src/api/allOrders.js";
-import { get } from "./src/api/api.js";
 import { loadFurniture } from "./src/api/loadFurniture.js";
 import { buyProducts } from "./src/views/buy.js";
 import { createRecipe } from "./src/views/create.js";
+import { logout } from "./src/views/logout.js";
 
 await loadFurniture(null, true);
 
-const logoutBtn = document.getElementById('logoutBtn');
-logoutBtn.addEventListener('click', logout);
-
-async function logout() {
-    await get('users/logout');
-    sessionStorage.removeItem('user');
-    location.href = './home.html';
-}
-
-const formElement = document.querySelector('form');
-formElement.addEventListener('submit', createRecipe);
-
-const orders = document.querySelector('.orders span');
-orders.textContent = '';
-
-const totalPrice = document.querySelectorAll('.orders span')[1];
-totalPrice.textContent = '';
-
-const buyBtn = document.getElementById('buyBtn');
-buyBtn.addEventListener('click', buyProducts);
+document.getElementById('logoutBtn').addEventListener('click', logout);
+document.querySelector('form').addEventListener('submit', createRecipe);
+document.querySelector('.orders span').textContent = '';
+document.querySelectorAll('.orders span')[1].textContent = '';
+document.getElementById('buyBtn').addEventListener('click', buyProducts);
 
 const userId = JSON.parse(sessionStorage.getItem('user'))._id;
-
-const allOrdersBtn = document.getElementById('allOrdersBtn');
-allOrdersBtn.addEventListener('click', () => showAllOrders(userId));
+document.getElementById('allOrdersBtn').addEventListener('click', () => showAllOrders(userId));
