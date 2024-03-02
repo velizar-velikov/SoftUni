79d127eaa39a1f381fae5ce66914d8f76c98dcbd
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-02 23:39:59 +0200

    remove links from app.js

--- jsAdvanced/JS Applications/03-Single-Page-Applications/Exercise/04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/app.js
+++ jsAdvanced/JS Applications/03-Single-Page-Applications/Exercise/04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/app.js
@@ -1,4 +1,5 @@
-import { logout, showSection } from "./util.js";
+import { onNavigate } from "./links.js";
+import { add } from "./views/addMovie.js";
 import { showHome } from "./views/home.js";
 import { login } from "./views/login.js";
 import { register } from "./views/register.js";
@@ -6,27 +7,14 @@ import { register } from "./views/register.js";
 
 showHome();
 
-const links = {
-    '/': showHome,
-    '/login': showSection.bind(null, 'form-login'),
-    '/register': showSection.bind(null, 'form-sign-up'),
-    '/logout': logout
-}
-
-
 document.getElementById('login-form').addEventListener('submit', login);
 document.getElementById('register-form').addEventListener('submit', register);
 
-document.querySelectorAll('nav a').forEach(link => {
+document.querySelector('#add-movie-form').addEventListener('submit', add);
+
+document.querySelectorAll('a').forEach(link => {
     if (link.id == 'welcome-msg') {
         return;
     }
     link.addEventListener('click', onNavigate);
 })
-
-function onNavigate(event) {
-    event.preventDefault();
-
-    const path = event.target.pathname;
-    links[path]();
-}
