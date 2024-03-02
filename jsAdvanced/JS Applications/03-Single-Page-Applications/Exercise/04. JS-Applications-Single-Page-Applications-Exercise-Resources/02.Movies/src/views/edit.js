6ede9fc58d244cd12606bf4c4a1ba715a87b9138
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-02 23:35:59 +0200

    Create edit.js

---
+++ jsAdvanced/JS Applications/03-Single-Page-Applications/Exercise/04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/views/edit.js
@@ -0,0 +1,38 @@
+import { showDetails } from "../components/moviePreview.js";
+import { get, put } from "../data/api.js";
+import { showSection } from "../util.js";
+
+const form = document.querySelector('#edit-movie form');
+
+async function edit(e, id) {
+    e.preventDefault();
+
+    const { title, description, img } = Object.fromEntries((new FormData(e.target)).entries());
+
+    const movieData = await put(`data/movies/${id}`, { title, description, img });
+    form.reset();
+
+    showDetails(movieData);
+}
+
+export async function loadForEdit(e) {
+
+    showSection('edit-movie');
+    const id = e.target.parentElement.parentElement.parentElement.parentElement.dataset.movieId;
+    console.log(id);
+    const movieData = await get(`data/movies/${id}`);
+
+    console.log(movieData);
+
+    const titleElement = document.querySelector('#edit-movie #title');
+    const descriptionElement = document.querySelector('#edit-movie [name="description"]');
+    const imgElement = document.querySelector('#edit-movie #imageUrl');
+
+    form.addEventListener('submit', (e) => edit(e, movieData._id));
+    debugger
+
+    //loading info for editing
+    titleElement.value = movieData.title;
+    descriptionElement.value = movieData.description;
+    imgElement.value = movieData.img;
+}
