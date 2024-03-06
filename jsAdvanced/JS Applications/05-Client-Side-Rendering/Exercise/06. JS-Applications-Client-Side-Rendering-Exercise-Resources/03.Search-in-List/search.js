3386f3b78630342b3abe5957e8cfeb553421455d
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-06 16:46:06 +0200

    03. Search in list solution

--- jsAdvanced/JS Applications/05-Client-Side-Rendering/Exercise/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
+++ jsAdvanced/JS Applications/05-Client-Side-Rendering/Exercise/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
@@ -1,3 +1,33 @@
+import { html, render } from "../node_modules/lit-html/lit-html.js";
+import { towns } from "./towns.js";
+
+document.querySelector('button').addEventListener('click', search);
+
+const searchInput = document.getElementById('searchText');
+const resultDiv = document.getElementById('result');
+
+const townsTemplate = (towns) => html`
+   <ul>
+      ${towns.map(town => html`<li>${town}</li>`)}
+   </ul>`;
+
+render(townsTemplate(towns), document.getElementById('towns'));
+
+
 function search() {
-   // TODO
+   let matchesCount = 0;
+   resultDiv.textContent = '';
+
+   document.querySelectorAll('#towns li').forEach(town => {
+
+      town.classList.remove('active');
+
+      if (town.textContent.includes(searchInput.value)) {
+         town.classList.add('active');
+         matchesCount++;
+      }
+   })
+   resultDiv.textContent = `${matchesCount} matches found`;
 }
+
+
