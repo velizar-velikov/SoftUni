4dc32410158c4f0974ae95519c24a10c55b9ac58
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-07 13:49:16 +0200

    add links structure in app.js

---
+++ jsAdvanced/JS Applications/Old Exams/JS Applications Retake Exam - 12 December 2023/Our-Luxury-Cars_Resources/src/app.js
@@ -0,0 +1,21 @@
+
+
+
+
+const views = {
+    'home-link': null,
+    'login-link': null,
+    'register-link': null,
+    'dashboard-link': null,
+    'add-link': null,
+    'delete-link': null,
+    'details-link': null,
+    'edit-link': null,
+    'search-link': null,
+    'logout-link': null
+};
+
+
+
+
+document.getElementById('main-element').remove();
