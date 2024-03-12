5b8b4bd2e181bac385d22bd23940839a383159dd
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-12 23:32:29 +0200

    added documentation for utils.js

--- jsAdvanced/JS Applications/Old Exams/JS Applications Regular Exam - 29 July 2023/Fun Facts-Resources/src/utils.js
+++ jsAdvanced/JS Applications/Old Exams/JS Applications Regular Exam - 29 July 2023/Fun Facts-Resources/src/utils.js
@@ -1,18 +1,43 @@
 const userItem = 'userData';
 
+/**
+ * Gets userdata from session storage
+ * @returns {Object} the parsed userdata object
+ */
 export const getUserData = () => {
     return JSON.parse(sessionStorage.getItem(userItem));
 }
 
+/**
+ * Sets userdata in session storage as json
+ * @param {Object} data userdata object
+ */
 export const setUserData = (data) => {
-    return sessionStorage.setItem(userItem, JSON.stringify(data));
+    sessionStorage.setItem(userItem, JSON.stringify(data));
 }
 
+/**
+ * Clears userdata from session storage
+ */
 export const clearUserData = () => {
     sessionStorage.removeItem(userItem);
 }
 
+
+/**
+ * A function that takes a function as a parameter
+ * and returns a function that extracts form data
+ * and calls the recieved function parameter with the form data and the form
+ * @param {Function} callback function that validates input fields,
+ * makes a request with the validated inputs and redirects to another page
+ * @returns {Function}
+ */
 export function createSubmitHandler(callback) {
+    /**
+     * extracts form data and calls the callback function seen from the closure
+     * @param {SubmitEvent} event
+     * @returns {void}
+     */
     return function (event) {
         event.preventDefault();
 
