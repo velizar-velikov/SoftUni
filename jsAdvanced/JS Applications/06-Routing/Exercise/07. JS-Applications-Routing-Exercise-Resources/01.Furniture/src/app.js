9a039ab61188da7714a22c30cb8c49d8400ebc83
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-14 13:28:16 +0200

    initialize project in app.js

---
+++ jsAdvanced/JS Applications/06-Routing/Exercise/07. JS-Applications-Routing-Exercise-Resources/01.Furniture/src/app.js
@@ -0,0 +1,42 @@
+import { logout } from './data/auth.js';
+import { page, render } from './lib.js';
+import { getUserData } from './utils.js';
+import { showCreatePage } from './views/create.js';
+import { showDashboardPage } from './views/dashboard.js';
+import { showDetailsPage } from './views/details.js';
+import { showEditPage } from './views/edit.js';
+import { layoutTemplate } from './views/layout.js';
+import { showLoginPage } from './views/login.js';
+import { showMyDashboardPage } from './views/my-dashboard.js';
+import { showRegisterPage } from './views/register.js';
+
+const root = document.querySelector('body');
+
+page(decorateContext);
+page('/index.html', '/');
+page('/', showDashboardPage);
+page('/dashboard', showDashboardPage);
+page('/my-dashboard', showMyDashboardPage);
+page('/details/:id', showDetailsPage);
+page('/details/:id/edit', showEditPage);
+page('/register', showRegisterPage);
+page('/login', showLoginPage);
+page('/create', showCreatePage);
+page('/logout', logoutAction);
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = renderView;
+    next();
+}
+
+function renderView(content) {
+    const userData = getUserData();
+    render(layoutTemplate(userData, content), root);
+}
+
+async function logoutAction(ctx) {
+    await logout();
+    ctx.page.redirect('/dashboard');
+}
