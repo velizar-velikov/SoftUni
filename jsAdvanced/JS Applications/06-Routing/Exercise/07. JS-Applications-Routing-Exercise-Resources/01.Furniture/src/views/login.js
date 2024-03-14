2ea73307057fe6706015b40d458f203a0855f5f4
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-14 13:27:09 +0200

    created login and register functionality

---
+++ jsAdvanced/JS Applications/06-Routing/Exercise/07. JS-Applications-Routing-Exercise-Resources/01.Furniture/src/views/login.js
@@ -0,0 +1,46 @@
+import { login } from '../data/auth.js';
+import { html } from '../lib.js';
+import { createSubmitHandler } from '../utils.js';
+
+const loginTemplate = (onLogin) => html`
+    <section id="login">
+        <div class="container">
+            <div class="row space-top">
+                <div class="col-md-12">
+                    <h1>Login User</h1>
+                    <p>Please fill all fields.</p>
+                </div>
+            </div>
+            <form @submit=${onLogin}>
+                <div class="row space-top">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="form-control-label" for="email">Email</label>
+                            <input class="form-control" id="email" type="text" name="email" />
+                        </div>
+                        <div class="form-group">
+                            <label class="form-control-label" for="password">Password</label>
+                            <input class="form-control" id="password" type="password" name="password" />
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Login" />
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+`;
+
+export function showLoginPage(ctx) {
+    ctx.render(loginTemplate(createSubmitHandler(onLogin)));
+
+    async function onLogin({ email, password }, form) {
+        if (email === '' || password === '') {
+            return alert('All fields are required!');
+        }
+
+        await login(email, password);
+
+        form.reset();
+        ctx.page.redirect('/dashboard');
+    }
+}
---
+++ jsAdvanced/JS Applications/06-Routing/Exercise/07. JS-Applications-Routing-Exercise-Resources/01.Furniture/src/views/register.js
@@ -0,0 +1,52 @@
+import { register } from '../data/auth.js';
+import { html } from '../lib.js';
+import { createSubmitHandler } from '../utils.js';
+
+const registerTemplate = (onRegister) => html`
+    <section id="register">
+        <div class="container">
+            <div class="row space-top">
+                <div class="col-md-12">
+                    <h1>Register New User</h1>
+                    <p>Please fill all fields.</p>
+                </div>
+            </div>
+            <form @submit=${onRegister}>
+                <div class="row space-top">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="form-control-label" for="email">Email</label>
+                            <input class="form-control" id="email" type="text" name="email" />
+                        </div>
+                        <div class="form-group">
+                            <label class="form-control-label" for="password">Password</label>
+                            <input class="form-control" id="password" type="password" name="password" />
+                        </div>
+                        <div class="form-group">
+                            <label class="form-control-label" for="rePass">Repeat</label>
+                            <input class="form-control" id="rePass" type="password" name="rePass" />
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Register" />
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+`;
+
+export function showRegisterPage(ctx) {
+    ctx.render(registerTemplate(createSubmitHandler(onRegister)));
+
+    async function onRegister({ email, password, rePass }, form) {
+        if (email === '' || password === '' || rePass === '') {
+            return alert('All fields are required!');
+        } else if (password !== rePass) {
+            return alert('Passwords must match!');
+        }
+
+        await register(email, password);
+        
+        form.reset();
+        ctx.page.redirect('/dashboard');
+    }
+}
