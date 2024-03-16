e6d0f3af837a21ffe50f956eaf5cc273810e4812
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-16 22:12:00 +0200

    created layout and home functionality

---
+++ jsAdvanced/JS Applications/07-Modular-Applications/Exercise/08.JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/src/views/layout.js
@@ -0,0 +1,17 @@
+import { html } from '../lib.js';
+
+export const layoutTemplate = (userData, content) => html`
+    <header id="titlebar" class="layout">
+        <a href="/" class="site-logo">Team Manager</a>
+        <nav>
+            <a href="/dashboard" class="action">Browse Teams</a>
+            ${userData
+                ? html` <a href="/my-dashboard" class="action">My Teams</a>
+                      <a href="/logout" class="action">Logout</a>`
+                : html` <a href="/login" class="action">Login</a>
+                      <a href="/register" class="action">Register</a>`}
+        </nav>
+    </header>
+    <main>${content}</main>
+    <footer id="footer">SoftUni &copy; 2014-2021</footer>
+`;
