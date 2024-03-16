190b8f9694aa455ad499641e749844eb49e19e7e
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-16 22:12:48 +0200

    created dashboard and my-dashboard functionality

---
+++ jsAdvanced/JS Applications/07-Modular-Applications/Exercise/08.JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/src/views/dashboard.js
@@ -0,0 +1,39 @@
+import { getAllTeams, getTeamMembers } from '../data/data.js';
+import { html } from '../lib.js';
+
+const dashboardTemplate = (teams) => html`
+    <section id="browse">
+        <article class="pad-med">
+            <h1>Team Browser</h1>
+        </article>
+
+        <article class="layout narrow">
+            <div class="pad-small"><a href="/create" class="action cta">Create Team</a></div>
+        </article>
+
+        ${teams.map(teamTemplate)}
+    </section>
+`;
+
+const teamTemplate = (team) => html`
+    <article class="layout">
+        <img src=${team.logoUrl} class="team-logo left-col" />
+        <div class="tm-preview">
+            <h2>${team.name}</h2>
+            <p>${team.description}</p>
+            <span class="details">${team.members} Members</span>
+            <div><a href="/details/${team._id}" class="action">See details</a></div>
+        </div>
+    </article>
+`;
+
+export async function showDashboardPage(ctx) {
+    const teams = await getAllTeams();
+
+    for (let team of teams) {
+        const teamMembers = await getTeamMembers(team._id);
+        team.members = teamMembers.filter((m) => m.status == 'member').length;
+        console.log(team);
+    }
+    ctx.render(dashboardTemplate(teams));
+}
---
+++ jsAdvanced/JS Applications/07-Modular-Applications/Exercise/08.JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/src/views/my-dashboard.js
@@ -0,0 +1,45 @@
+import { getMyTeamMemberShips, getTeamMembers } from '../data/data.js';
+import { html } from '../lib.js';
+import { getUserData } from '../utils/userHelper.js';
+
+const myDashboardTemplate = (teams) => html`
+    <section id="my-teams">
+        <article class="pad-med">
+            <h1>My Teams</h1>
+        </article>
+
+        <article class="layout narrow">
+            <div class="pad-med">
+                <p>You are not a member of any team yet.</p>
+                <p><a href="/dashboard">Browse all teams</a> to join one, or use the button bellow to create your own team.</p>
+            </div>
+            <div class=""><a href="/create" class="action cta">Create Team</a></div>
+        </article>
+
+        ${teams.map(teamTemplate)}
+    </section>
+`;
+
+const teamTemplate = ({ team }) => html`
+    <article class="layout">
+        <img src=${team.logoUrl} class="team-logo left-col" />
+        <div class="tm-preview">
+            <h2>${team.name}</h2>
+            <p>${team.description}</p>
+            <span class="details">${team.members} Members</span>
+            <div><a href="/details/${team._id}" class="action">See details</a></div>
+        </div>
+    </article>
+`;
+
+export async function showMyDashboard(ctx) {
+    const userId = getUserData()._id;
+    const teams = await getMyTeamMemberShips(userId);
+
+    for (let { team } of teams) {
+        const teamMembers = await getTeamMembers(team._id);
+        team.members = teamMembers.filter((m) => m.status == 'member').length;
+    }
+
+    ctx.render(myDashboardTemplate(teams));
+}
