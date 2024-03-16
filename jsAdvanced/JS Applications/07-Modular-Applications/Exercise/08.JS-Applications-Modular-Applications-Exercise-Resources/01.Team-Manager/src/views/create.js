9e9b99d8feea32a8dbbabdb274a9f827fb184f56
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-16 22:14:17 +0200

    created edit and edit functionality

---
+++ jsAdvanced/JS Applications/07-Modular-Applications/Exercise/08.JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/src/views/create.js
@@ -0,0 +1,61 @@
+import { createTeam } from '../data/data.js';
+import { html } from '../lib.js';
+import { createSubmitHandler } from '../utils/formHelper.js';
+
+const createTemplate = (onCreate, errorMessage) => html`
+    <section id="create">
+        <article class="narrow">
+            <header class="pad-med">
+                <h1>New Team</h1>
+            </header>
+            <form @submit=${onCreate} id="create-form" class="main-form pad-large">
+                ${errorMessage ? html` <div class="error">${errorMessage}</div>` : null}
+                <label>Team name: <input type="text" name="name" /></label>
+                <label>Logo URL: <input type="text" name="logoUrl" /></label>
+                <label>Description: <textarea name="description"></textarea></label>
+                <input class="action cta" type="submit" value="Create Team" />
+            </form>
+        </article>
+    </section>
+`;
+
+export function showCreatePage(ctx) {
+    let data;
+
+    async function onCreate(formData, form) {
+        data = formData;
+        const { name, logoUrl, description } = formData;
+
+        let errorMessage = getErrorMessage();
+
+        if (errorMessage) {
+            ctx.render(createTemplate(createSubmitHandler(onCreate), errorMessage));
+        } else {
+            try {
+                const res = await createTeam(name, logoUrl, description);
+
+                if (!res.message) {
+                    form.reset();
+                    ctx.goTo(`/details/${res._id}`);
+                }
+            } catch (error) {
+                ctx.render(createTemplate(createSubmitHandler(onCreate), error.message));
+            }
+        }
+    }
+
+    ctx.render(createTemplate(createSubmitHandler(onCreate)));
+
+    function getErrorMessage() {
+        let errorMessage;
+        if (data.name.length < 4) {
+            errorMessage = 'Team name should be at least 4 characters long.';
+        } else if (data.logoUrl.length === 0) {
+            errorMessage = 'Logo URL is required.';
+        } else if (data.description.length < 10) {
+            errorMessage = 'Description should be at least 10 characters long.';
+        }
+
+        return errorMessage;
+    }
+}
---
+++ jsAdvanced/JS Applications/07-Modular-Applications/Exercise/08.JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/src/views/edit.js
@@ -0,0 +1,66 @@
+import { editTeam, getTeamById } from '../data/data.js';
+import { html } from '../lib.js';
+import { createSubmitHandler } from '../utils/formHelper.js';
+
+const editTemplate = (onEdit, team, errorMessage) => html`
+    <section id="edit">
+        <article class="narrow">
+            <header class="pad-med">
+                <h1>Edit Team</h1>
+            </header>
+            <form @submit=${onEdit} id="edit-form" class="main-form pad-large">
+                ${errorMessage ? html` <div class="error">${errorMessage}</div>` : null}
+
+                <label>Team name: <input .value=${team.name} type="text" name="name" /></label>
+                <label>Logo URL: <input .value=${team.logoUrl} type="text" name="logoUrl" /></label>
+                <label>Description: <textarea .value=${team.description} name="description"></textarea></label>
+                <input class="action cta" type="submit" value="Save Changes" />
+            </form>
+        </article>
+    </section>
+`;
+
+export async function showEditPage(ctx) {
+    const id = ctx.params.id;
+
+    const team = await getTeamById(id);
+
+    let data;
+
+    async function onEdit(formData, form) {
+        data = formData;
+        const { name, logoUrl, description } = formData;
+
+        let errorMessage = getErrorMessage();
+
+        if (errorMessage) {
+            ctx.render(editTemplate(createSubmitHandler(onEdit), team, errorMessage));
+        } else {
+            try {
+                const res = await editTeam(team._id, name, logoUrl, description);
+
+                if (!res.message) {
+                    form.reset();
+                    ctx.goTo(`/details/${res._id}`);
+                }
+            } catch (error) {
+                ctx.render(editTemplate(createSubmitHandler(onEdit), team, error.message));
+            }
+        }
+    }
+
+    ctx.render(editTemplate(createSubmitHandler(onEdit), team));
+
+    function getErrorMessage() {
+        let errorMessage;
+        if (data.name.length < 4) {
+            errorMessage = 'Team name should be at least 4 characters long.';
+        } else if (data.logoUrl.length === 0) {
+            errorMessage = 'Logo URL is required.';
+        } else if (data.description.length < 10) {
+            errorMessage = 'Description should be at least 10 characters long.';
+        }
+
+        return errorMessage;
+    }
+}
