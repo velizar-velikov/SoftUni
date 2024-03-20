0de29e411936327d817aff78850c033eaa14b883
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-20 12:51:33 +0200

    created middlewares for rendering and redirecting

---
+++ jsAdvanced/JS Applications/Old Exams/JS Applications Regular Exam - 1 April 2023/Fruitipedia/src/middlewares/renderMiddleware.js
@@ -0,0 +1,15 @@
+import { render } from '../lib/lib.js';
+import { getUserData } from '../utils/userHelper.js';
+import { layoutTemplate } from '../views/layout.js';
+
+const root = document.getElementById('wrapper');
+
+export function addRender(ctx, next) {
+    ctx.render = renderView;
+    next();
+}
+
+function renderView(content) {
+    const userData = getUserData();
+    render(layoutTemplate(userData, content), root);
+}
