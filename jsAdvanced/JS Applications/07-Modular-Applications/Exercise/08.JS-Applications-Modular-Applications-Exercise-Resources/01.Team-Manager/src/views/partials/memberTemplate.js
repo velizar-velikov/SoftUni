76e7736fbf4a0fe1a0d8dfcf3beadcd23e082fa9
velizar-velikov <vvelikov7.22@gmail.com>
2024-03-25 01:11:52 +0200

    added event listener to the templates for member and request

--- jsAdvanced/JS Applications/07-Modular-Applications/Exercise/08.JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/src/views/partials/memberTemplate.js
+++ jsAdvanced/JS Applications/07-Modular-Applications/Exercise/08.JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/src/views/partials/memberTemplate.js
@@ -1,10 +1,10 @@
 import { html } from '../../lib/lib.js';
 
-export const memberTemplate = (member, userData, isOwner) => html`<li>
+export const memberTemplate = (member, userData, isOwner, onRemove) => html`<li>
     ${member.user.username}
     ${member._ownerId !== userData?._id && isOwner
         ? html`
-          <a href="javascript:void(0)" class="tm-control action">Remove from team</a>
+          <a href="javascript:void(0)" class="tm-control action" @click=${onRemove}>Remove from team</a>
       </li>`
         : null}
 </li> `;
--- jsAdvanced/JS Applications/07-Modular-Applications/Exercise/08.JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/src/views/partials/requestTemplate.js
+++ jsAdvanced/JS Applications/07-Modular-Applications/Exercise/08.JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/src/views/partials/requestTemplate.js
@@ -1,9 +1,9 @@
 import { html } from '../../lib/lib.js';
 
-export const requestTemplate = (member) => html`
+export const requestTemplate = (member, onApprove, onDecline) => html`
     <li>
         ${member.user.username}
-        <a href="javascript:void(0)" class="tm-control action">Approve</a>
-        <a href="javascript:void(0)" class="tm-control action">Decline</a>
+        <a href="javascript:void(0)" class="tm-control action" @click=${onApprove}>Approve</a>
+        <a href="javascript:void(0)" class="tm-control action" @click=${onDecline}>Decline</a>
     </li>
 `;
