da49198cb0e18573a899f27ce8eddf4691882a60
Velizar Velikov <vvelikov7.22@gmail.com>
2024-04-04 00:03:40 +0300

    changes

---
+++ TypeScript/Introduction to TypeScript/test/test.js
@@ -0,0 +1,7 @@
+"use strict";
+function test(myVar, secVar, third) {
+    return `${myVar} is ${secVar} years old - ${third} ha-ha`;
+}
+const result = test('Velizar', 23, true);
+console.log(result);
+//# sourceMappingURL=test.js.map
