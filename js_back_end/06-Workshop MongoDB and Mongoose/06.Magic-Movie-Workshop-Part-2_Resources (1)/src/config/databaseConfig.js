1443bc5c75a764d2e74409667fa0bda7795a808e
velizar-velikov <vvelikov7.22@gmail.com>
2024-11-11 12:37:04 +0200

    config mongoose database

---
+++ js_back_end/06-Workshop MongoDB and Mongoose/06.Magic-Movie-Workshop-Part-2_Resources (1)/src/config/databaseConfig.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('../models/Movie.js');
+require('../models/Cast.js');
+
+async function databaseConfig() {
+    const connectionStr = 'mongodb://127.0.0.1:27017/movies';
+
+    mongoose.connection.on('connected', () => {
+        console.log('Mongoose connected to ' + connectionStr);
+    });
+    mongoose.connection.on('error', (err) => {
+        console.log('Mongoose error connection: ' + err);
+    });
+
+    await mongoose.connect(connectionStr);
+}
+
+module.exports = { databaseConfig };
