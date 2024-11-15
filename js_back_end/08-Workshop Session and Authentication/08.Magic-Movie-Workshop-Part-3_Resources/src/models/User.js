8a9785bbf2a955057d69c29040ad8bab8ec2bd09
velizar-velikov <vvelikov7.22@gmail.com>
2024-11-15 17:25:17 +0200

    case insensitive user data to the database

--- js_back_end/08-Workshop Session and Authentication/08.Magic-Movie-Workshop-Part-3_Resources/src/models/User.js
+++ js_back_end/08-Workshop Session and Authentication/08.Magic-Movie-Workshop-Part-3_Resources/src/models/User.js
@@ -1,15 +1,21 @@
 const { Schema, SchemaType, model } = require('mongoose');
 
-const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
+const userSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    password: {
-        type: String,
-        required: true,
-    },
-});
+    {
+        // case insensitive
+        collation: { locale: 'en', strength: 2 },
+    }
+);
 
 const User = model('User', userSchema);
 
