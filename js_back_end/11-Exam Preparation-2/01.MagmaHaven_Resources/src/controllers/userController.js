1f27bf667ba535403ae11f313910dc7902a6367c
velizar-velikov <vvelikov7.22@gmail.com>
2024-11-29 16:28:54 +0200

    code clean up and added comments

--- js_back_end/11-Exam Preparation-2/01.MagmaHaven_Resources/src/controllers/userController.js
+++ js_back_end/11-Exam Preparation-2/01.MagmaHaven_Resources/src/controllers/userController.js
@@ -13,7 +13,7 @@ const registerController = {
         res.render('register');
     },
     post: async (req, res) => {
-        const { username, email, password, repass } = req.body;
+        const { username, email, password } = req.body;
 
         const result = await validationResult(req);
 
@@ -23,8 +23,7 @@ const registerController = {
             }
             const user = await register(username, email, password);
             const token = createToken(user);
-            // expires in one day after it has been created
-            res.cookie('token', token);
+            res.cookie('token', token, { httpOnly: true });
             res.redirect('/');
         } catch (err) {
             res.render('register', { data: req.body, errors: parseError(err).errors });
@@ -44,8 +43,14 @@ const loginController = {
         try {
             const user = await login(email, password);
             const token = createToken(user);
-            // expires in one day after it has been created
-            res.cookie('token', token);
+            // these cookie flags ensure secure usage of cookies, preventing from XSS attacks
+
+            // secure => true -> cookie is sent from the browser only for https requests
+            // samesite => 'Strict' -> cookie is sent from the browser only to the site that is created from
+            // httpOnly => true -> cookie can only be sent via the browser for http communication,
+            //  cannot be read from the client via document.cookie
+
+            res.cookie('token', token, { httpOnly: true });
             res.redirect('/');
         } catch (err) {
             res.render('login', { data: req.body, errors: parseError(err).errors });
