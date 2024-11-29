290c7b6ce525945cf0f122f85b7e5b576b3df331
velizar-velikov <vvelikov7.22@gmail.com>
2024-11-29 02:38:58 +0200

    add useState

--- React/01. Introduction to React and JSX/src/components/Hero.jsx
+++ React/01. Introduction to React and JSX/src/components/Hero.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 export default function Hero() {
+    const [count, setCount] = useState(0);
+
+    setTimeout(() => {
+        setCount(count + 1);
+    }, 1000);
+
     return (
         <div className="container">
             <div className="welcome-hero-txt">
-                <h2>get your desired car in resonable price</h2>
+                <h2>get your desired car in resonable price {count}</h2>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
                     magna aliqua.
