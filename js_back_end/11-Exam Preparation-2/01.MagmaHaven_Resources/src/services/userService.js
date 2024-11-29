0aa047f35aa00d8004e0a3610698f62c4f8e3d89
velizar-velikov <vvelikov7.22@gmail.com>
2024-11-29 02:35:30 +0200

    add user functionality

---
+++ js_back_end/11-Exam Preparation-2/01.MagmaHaven_Resources/src/services/userService.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcrypt');
+
+const { User } = require('../models/User.js');
+
+const saltRounds = 10;
+
+async function register(username, email, password) {
+    const existing = await User.findOne({ email });
+
+    if (existing) {
+        throw new Error('User with the same email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = new User({
+        username,
+        email,
+        password: hashedPassword,
+    });
+
+    await user.save();
+
+    return user;
+}
+
+async function login(email, password) {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Incorrect email or password');
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+        throw new Error('Incorrect email or password');
+    }
+
+    return user;
+}
+
+module.exports = { register, login };
