1c1bfa5635ddacbd0840c39b3f0c91bab646af19
velizar-velikov <vvelikov7.22@gmail.com>
2024-11-30 17:51:41 +0200

    add MainContent

---
+++ React/03.Components Deep Dive/todo-list/client/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Navigation from './components/Navigation.jsx';
+import Footer from './components/Footer.jsx';
+import MainContent from './components/MainContent.jsx';
+
+function App() {
+    return (
+        <>
+            <Navigation />
+
+            <MainContent />
+
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ React/03.Components Deep Dive/todo-list/client/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+import ToDo from './ToDo/ToDo.jsx';
+
+export default function MainContent() {
+    return (
+        <main className="main">
+            <ToDo />
+        </main>
+    );
+}
