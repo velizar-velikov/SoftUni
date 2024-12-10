5f970347303869e1aa0c9d33ae683afab69611d3
velizar-velikov <vvelikov7.22@gmail.com>
2024-12-10 21:32:33 +0200

    define routes in app.jsx

--- React/05.Routing/client/src/App.jsx
+++ React/05.Routing/client/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NotFound from './components/404.jsx';
+import About from './components/About.jsx';
+import Comments from './components/Comments.jsx';
+import Dashboard from './components/Dashboard.jsx';
+import Details from './components/Details.jsx';
+import Footer from './components/Footer.jsx';
+import Home from './components/Home.jsx';
+import Navigation from './components/Navigation.jsx';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="flex flex-col min-h-screen">
+            <BrowserRouter>
+                <Navigation />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/details/:id" element={<Details />}>
+                        <Route path="comments" element={<Comments />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
